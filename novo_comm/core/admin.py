b38b6eb75f90398131a2bf5e70528e2845ed4b8a
Daniel <danielprocopio15@gmail.com>
2025-10-24 16:42:36 -0300

    FEAT/ algoritmo de acordo com cliques + página home

--- novo_comm/core/admin.py
+++ novo_comm/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ThemeAccess
 
-# Register your models here.
+
+@admin.register(ThemeAccess)
+class ThemeAccessAdmin(admin.ModelAdmin):
+	list_display = ("user", "category", "count", "updated_at")
+	search_fields = ("user__username", "category")
+	list_filter = ("category",)
